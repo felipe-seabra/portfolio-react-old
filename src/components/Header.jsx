d5aabc381c7f025d52a8bd05e9f74c172aece045
Felipe Seabra <feliperoce@gmail.com>
2022-08-29 20:33:55 -0300

    Ajustes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,43 +6,41 @@ class Header extends React.Component {
     const linkedin = "https://www.linkedin.com/in/felipe-seabra";
 
     return (
-      <header className="fixed-top">
-        <nav className="navbar navbar-expand-md container-fluid">
-          <h2 className="logo navbar-brand">
-            Portfó<span>lio</span>
-          </h2>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-felipe" aria-controls="nav-felipe" aria-expanded="false" aria-label="Alterna navegação">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-center" id="nav-felipe">
-            <ul className="cabeçalho-link navbar-nav">
-              <li className="nav-item active">
-                <Link to="/" className="nav-link" >
-                  Home<span className="sr-only"></span>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/about" className="nav-link">
-                  Sobre
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/projects" className="nav-link">
-                  Projetos
-                </Link>
-              </li>
-              {/* <li className="nav-item">
+      <nav className="navbar navbar-expand-md container-fluid">
+        <h2 className="logo navbar-brand">
+          Portfó<span>lio</span>
+        </h2>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-felipe" aria-controls="nav-felipe" aria-expanded="false" aria-label="Alterna navegação">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-center" id="nav-felipe">
+          <ul className="cabeçalho-link navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home<span className="sr-only"></span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                Sobre
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/projects" className="nav-link">
+                Projetos
+              </Link>
+            </li>
+            {/* <li className="nav-item">
                 <a className="nav-link" href="/contact">
                   Contato
                 </a>
               </li> */}
-            </ul>
-          </div>
-          <a target="_blank" className="btn d-none d-lg-block" href={linkedin} rel="noreferrer">
-            Linkedin
-          </a>
-        </nav>
-      </header>
+          </ul>
+        </div>
+        <a target="_blank" className="btn d-none d-lg-block" href={linkedin} rel="noreferrer">
+          Linkedin
+        </a>
+      </nav>
     );
   }
 }
