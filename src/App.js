f1a147210bd9ecaf987983d09e67d3659d3fa7a3
Felipe Seabra <feliperoce@gmail.com>
2022-08-29 22:00:18 -0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
