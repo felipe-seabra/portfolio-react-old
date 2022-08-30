4e760cb400bea1fd68af8abf138506f3b2fad109
Felipe Seabra <feliperoce@gmail.com>
2022-08-29 22:54:47 -0300

    adicionando repositórios nos projetos

--- src/backend/data.js
+++ src/backend/data.js
@@ -6,13 +6,13 @@ const data = [
     name: "Projeto Pixels Art",
     image: pixelArt,
     description: "Projeto desenvolvido após as aulas de JavaScript, onde aprendemos a trabalhar com elementos, eventos e web storage.",
-    url: "",
+    url: "https://github.com/felipe-seabra/Trybe-project-pixels-art",
   },
   {
     name: "Página Colégio Criarte",
     image: criarte,
     description: "Projeto desenvolvido para descrição dos cursos profissionalizantes do Colégio Criarte. HTML, CSS e JavaScript.",
-    url: "",
+    url: "https://github.com/felipe-seabra/criarteProfissionalizante",
   },
 ];
 
