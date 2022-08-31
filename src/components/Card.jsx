84f274d31ff6d545ff6732eb8ae6dd7566a6ef98
Felipe Seabra <feliperoce@gmail.com>
2022-08-31 18:35:30 -0300

    Primeira implementação do Loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import Loading from "./Loading";
 
 export default class Card extends React.Component {
+  state = {
+    loading: true,
+  };
   render() {
     const { name, image, description, url } = this.props;
     return (
       <div className="card">
-        <div className="card-bg">
-          <img className="card-img-top" src={image} alt={name} />
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <p className="card-text">{description}</p>
-            <a href={url} target="_blank" className="btn" rel="noreferrer">
-              Visitar
-            </a>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="card-bg">
+            <img className="card-img-top" src={image} alt={name} />
+            <div className="card-body">
+              <h5 className="card-title">{name}</h5>
+              <p className="card-text">{description}</p>
+              <a href={url} target="_blank" className="btn" rel="noreferrer">
+                Visitar
+              </a>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
