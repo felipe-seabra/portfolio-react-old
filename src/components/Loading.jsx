7546d1eb663aea9a1e694b1c49ece91de8529969
Felipe Seabra <feliperoce@gmail.com>
2022-08-31 15:27:22 -0300

    css Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Loading extends React.Component {
   render() {
     return (
-      <span>Carregando...</span>
+      <div className='c-loader'/>
     );
   }
 }
