93bd42a6dbe6114b6eed5c63a8f40fc7b269e28d
Felipe Seabra <feliperoce@gmail.com>
2022-09-01 12:37:35 -0300

    EsLint

--- src/backend/data.js
+++ src/backend/data.js
@@ -1,31 +1,35 @@
 import pixelArt from "../images/projects/background-pixels-art.png";
-import shoppingCart from "../images/projects/background-shopping-cart.png"
-import trybewarts from "../images/projects/background-project-trybewarts.png"
+import shoppingCart from "../images/projects/background-shopping-cart.png";
+import trybewarts from "../images/projects/background-project-trybewarts.png";
 import criarte from "../images/projects/background-criarte-profissionalizante.png";
 
 const data = [
   {
     name: "Shopping Cart",
     image: shoppingCart,
-    description: "Projeto de desenvolvimento de um carrinho de compras utilizando a API do Mercado Livre. 🛒💻📱",
+    description:
+      "Projeto de desenvolvimento de um carrinho de compras utilizando a API do Mercado Livre. 🛒💻📱",
     url: "https://github.com/felipe-seabra/Trybe-project-shopping-cart",
   },
   {
     name: "Trybewarts",
     image: trybewarts,
-    description: "Projeto de desenvolvimento de formulário da Escola de Magia de Trybewarts. 🧙🪄⚡",
+    description:
+      "Projeto de desenvolvimento de formulário da Escola de Magia de Trybewarts. 🧙🪄⚡",
     url: "https://github.com/felipe-seabra/Trybe-project-trybewarts",
   },
   {
     name: "Pixels Art",
     image: pixelArt,
-    description: "Projeto de desenvolvimento de uma paleta de cores para criar desenhos em pixels. 🎨🖌",
+    description:
+      "Projeto de desenvolvimento de uma paleta de cores para criar desenhos em pixels. 🎨🖌",
     url: "https://github.com/felipe-seabra/Trybe-project-pixels-art",
   },
   {
     name: "Página Colégio Criarte",
     image: criarte,
-    description: "Projeto desenvolvido para descrição dos cursos profissionalizantes do Colégio Criarte.",
+    description:
+      "Projeto desenvolvido para descrição dos cursos profissionalizantes do Colégio Criarte.",
     url: "https://github.com/felipe-seabra/criarteProfissionalizante",
   },
 ];
