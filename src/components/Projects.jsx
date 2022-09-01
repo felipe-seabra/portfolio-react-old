f29c67c83a950bc208bff5ccc4293368e0b00b0c
Felipe Seabra <feliperoce@gmail.com>
2022-09-01 12:25:25 -0300

    Inserindo Loading nos cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,19 @@ import Card from "./Card";
 import data from "../backend/data";
 
 export default class Projects extends React.Component {
+  state = {
+    isLoaded: false,
+  };
+
+  componentDidMount() {
+    setTimeout(async() => {
+      await this.setState({isLoaded: true});
+    },1000)
+    
+  }
+
   render() {
+    const { isLoaded } = this.state;
     return (
       <div className="background-color-grey container-fluid">
         <div className="title">
@@ -17,7 +29,7 @@ export default class Projects extends React.Component {
               name={card.name}
               description={card.description}
               url={card.url}
-              isLoaded={true}
+              isLoaded={isLoaded}
             />
           ))}
         </div>
