0eedf6af094b9f4827fdad54998a6ac6d70e2357
Felipe Seabra <feliperoce@gmail.com>
2022-09-02 19:25:02 -0300

    upgrade css

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './Projects.module.css'
 import Card from "./Card";
 import data from "../backend/data";
 
@@ -10,27 +11,29 @@ export default class Projects extends React.Component {
   componentDidMount() {
     setTimeout(async () => {
       await this.setState({ isLoaded: true });
-    }, 1000);
+    }, 300);
   }
 
   render() {
     const { isLoaded } = this.state;
     return (
       <div className="background-color-grey container-fluid">
-        <div className="title">
+        <div className={styles.title}>
           <h2>Meus Projetos</h2>
         </div>
         <div className="card-box">
-          {data.map((card) => (
-            <Card
-              key={card.name}
-              image={card.image}
-              name={card.name}
-              description={card.description}
-              url={card.url}
-              isLoaded={isLoaded}
-            />
-          ))}
+          {data.length < 1
+            ? <p>Nenhum projeto encontrado</p>
+            : data.map((card) => (
+                <Card
+                  key={card.name}
+                  image={card.image}
+                  name={card.name}
+                  description={card.description}
+                  url={card.url}
+                  isLoaded={isLoaded}
+                />
+              ))}
         </div>
       </div>
     );
