744d3aa6536d50fa47a4faf6e0affb1ec5effbe2
Felipe Seabra <feliperoce@gmail.com>
2022-09-15 22:13:03 -0300

    page 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import './Error404.css'
 
 export default class Error404 extends React.Component {
   render() {
     return (
       <div className="background-color-grey">
-        <h1>Error 404</h1>
+        <div class="stars">
+
+            <div class="central-body">
+                <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px" alt="404"/>
+                <a href="/" class="btn-go-home">GO HOME</a>
+            </div>
+            <div class="objects">
+                <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px" alt="Rocket"/>
+                <div class="earth-moon">
+                    <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px" alt="Terra"/>
+                    <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px" alt="Lua"/>
+                </div>
+                <div class="box_astronaut">
+                    <img class="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px" alt="Astronalta"/>
+                </div>
+            </div>
+            <div class="glowing_stars">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+            </div>
+        </div>
       </div>
     );
   }
