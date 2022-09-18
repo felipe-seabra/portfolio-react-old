e24036ff012ea1bee5edc2a8478e1e00b2795c30
Felipe Seabra <feliperoce@gmail.com>
2022-09-17 21:54:08 -0300

    fix: eslint

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,22 +9,19 @@ class About extends React.Component {
       <section className="background-color-grey">
         <div className="container">
           <div className="main-sobre row">
-            <img className="picture img-fluid" src={ image } alt="Foto Felipe Seabra" />
+            <img className="picture img-fluid" src={image} alt="Foto Felipe Seabra" />
             <div className="Text-sobre mt-3">
               <h2>Sobre Mim</h2>
               <p>
-                Natural de Presidente Prudente (SP), tenho aproximadamente 10 anos de
-                experiência atuando em diversas áreas de tecnologia em educação.
+                Natural de Presidente Prudente (SP), tenho aproximadamente 10 anos de experiência
+                atuando em diversas áreas de tecnologia em educação.
               </p>
               <p>
-                Apesar de hoje me identificar mais com o
-                <strong>Front-end</strong>
-                , vejo
-                que não há nada que eu não consiga
-                <em>fazer/aprender</em>
-                .
+                Apesar de hoje me identificar mais com o<strong>Front-end</strong>, vejo que não há
+                nada que eu não consiga
+                <em>fazer/aprender</em>.
               </p>
-              <a className="btn" target="_blank" href={ linkedin } rel="noreferrer">
+              <a className="btn" target="_blank" href={linkedin} rel="noreferrer">
                 Vamos conversar
               </a>
             </div>
