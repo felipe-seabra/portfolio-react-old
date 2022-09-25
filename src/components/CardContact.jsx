c088bcd46ef2dc8ff214f53063c292793c5fdb8a
Felipe Seabra <feliperoce@gmail.com>
2022-09-25 11:53:57 -0300

    create setup CardContact

---
+++ src/components/CardContact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CardContact extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>CardContact</p>
+      </div>
+    );
+  }
+}
