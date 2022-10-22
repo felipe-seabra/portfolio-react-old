5a5f6b6559d0f217c0f2b20dbe4bf180725e95b8
Felipe Seabra <feliperoce@gmail.com>
2022-10-21 22:25:32 -0300

    CookieConsent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import CookieConsent from 'react-cookie-consent';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
@@ -10,6 +11,22 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Header />
+        <CookieConsent
+          // debug
+          style={
+            { background: '#000',
+              textAlign: 'left',
+              fontSize: '16px',
+              padding: '20px' }
+          }
+          buttonText="Aceitar"
+          expires={ 30 }
+          buttonClasses="btn"
+        >
+          Este site utiliza cookies para seu adequado funcionamento,
+          análises, personalização e publicidade.
+          Ao continuar navegando neste site você declara estar ciente destas condições.
+        </CookieConsent>
         <main id="about">
           <Content />
         </main>
