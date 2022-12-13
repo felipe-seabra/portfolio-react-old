7a9d6f4204d4c28aa386c47da54cce64b4a05d55
Felipe Seabra <feliperoce@gmail.com>
2022-12-13 11:57:15 -0300

    refactoring: Footer to function

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,46 +1,42 @@
 import React from 'react';
 import './style/Footer.css';
 
-class Footer extends React.Component {
-  render() {
-    const instagram = 'https://www.instagram.com/felipeseabra_';
-    const linkedin = 'https://www.linkedin.com/in/felipe-seabra';
-    const github = 'https://github.com/felipe-seabra';
+function Footer() {
+  const instagram = 'https://www.instagram.com/felipeseabra_';
+  const linkedin = 'https://www.linkedin.com/in/felipe-seabra';
+  const github = 'https://github.com/felipe-seabra';
 
-    return (
-      <footer>
-        {/* <h6>Felipe Seabra</h6> */}
-        {/* <p>Para mais informações - clique nos links abaixo</p> */}
-        <div className="social-networks">
-          <a
-            target="_blank"
-            href={ github }
-            rel="noreferrer"
-            style={ { textDecoration: 'none' } }
-          >
-            <i className="fab fa-github" />
-          </a>
-          <a
-            target="_blank"
-            href={ linkedin }
-            rel="noreferrer"
-            style={ { textDecoration: 'none' } }
-          >
-            <i className="fab fa-linkedin" />
-          </a>
-          <a
-            target="_blank"
-            href={ instagram }
-            rel="noreferrer"
-            style={ { textDecoration: 'none' } }
-          >
-            <i className="fab fa-instagram" />
-          </a>
-        </div>
-        <p className="end">Felipe S. - &copy; 2022</p>
-      </footer>
-    );
-  }
+  return (
+    <footer>
+      <div className="social-networks">
+        <a
+          target="_blank"
+          href={ github }
+          rel="noreferrer"
+          style={ { textDecoration: 'none' } }
+        >
+          <i className="fab fa-github" />
+        </a>
+        <a
+          target="_blank"
+          href={ linkedin }
+          rel="noreferrer"
+          style={ { textDecoration: 'none' } }
+        >
+          <i className="fab fa-linkedin" />
+        </a>
+        <a
+          target="_blank"
+          href={ instagram }
+          rel="noreferrer"
+          style={ { textDecoration: 'none' } }
+        >
+          <i className="fab fa-instagram" />
+        </a>
+      </div>
+      <p className="end">Felipe S. - &copy; 2022</p>
+    </footer>
+  );
 }
 
 export default Footer;
