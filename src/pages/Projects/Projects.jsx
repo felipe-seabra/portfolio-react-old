bce08b8611c7b2b63efcd443465e330fc4399324
Felipe Seabra <feliperoce@gmail.com>
2022-12-13 11:55:50 -0300

    refactoring: Projects to function

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,43 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style/Projects.css';
 import Card from '../../components/Card';
 import data from '../../data';
 
-export default class Projects extends React.Component {
-  state = {
-    isLoaded: false,
-  };
+export default function Projects() {
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  componentDidMount() {
+  useEffect(() => {
     document.title = 'Projetos - Felipe Seabra';
     const num = 400;
-    setTimeout(async () => {
-      await this.setState({ isLoaded: true });
+    setTimeout(() => {
+      setIsLoaded(true);
     }, num);
-  }
+  });
 
-  render() {
-    const { isLoaded } = this.state;
-    return (
-      <div className="background-color-grey container-fluid">
-        <div className="title">
-          <h2>Meus Projetos</h2>
-        </div>
-        <div className="card-box">
-          {data.length < 1
-            ? <p>Nenhum projeto encontrado</p>
-            : data.map((card) => (
-              <Card
-                key={ card.name }
-                image={ card.image }
-                name={ card.name }
-                description={ card.description }
-                url={ card.url }
-                isLoaded={ isLoaded }
-              />
-            ))}
-        </div>
+  return (
+    <div className="background-color-grey container-fluid">
+      <div className="title">
+        <h2>Meus Projetos</h2>
       </div>
-    );
-  }
+      <div className="card-box">
+        {data.length < 1
+          ? <p>Nenhum projeto encontrado</p>
+          : data.map((card) => (
+            <Card
+              key={ card.name }
+              image={ card.image }
+              name={ card.name }
+              description={ card.description }
+              url={ card.url }
+              isLoaded={ isLoaded }
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
