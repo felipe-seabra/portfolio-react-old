da448d8132e73a4ba216dbec564a044df7655017
Felipe Seabra <feliperoce@gmail.com>
2022-12-13 12:03:13 -0300

    refactoring: Home to function

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style/Home.css';
 
-export default class Main extends React.Component {
-  componentDidMount() {
+export default function Main() {
+  useEffect(() => {
     document.title = 'Felipe Seabra - Desenvolvedor Web';
-  }
-
-  render() {
-    return (
-      <div className="background-home container-fluid">
-        <div className="title-home">
-          <h4>Olá, meu nome é</h4>
-          <h1>
-            Felipe
-            {' '}
-            <span>Seabra</span>
-            <span id="animate-flicker">|</span>
-          </h1>
-          <h3>
-            Sou estudante de desenvolvimento web na
-            {' '}
-            <strong>
-              <a className="trybe" href="https://www.betrybe.com/" target="_blank" rel="noreferrer" style={ { textDecoration: 'none' } }>
-                Trybe
-              </a>
-            </strong>
-            .
-          </h3>
-        </div>
+  });
+  return (
+    <div className="background-home container-fluid">
+      <div className="title-home">
+        <h4>Olá, meu nome é</h4>
+        <h1>
+          Felipe
+          {' '}
+          <span>Seabra</span>
+          <span id="animate-flicker">|</span>
+        </h1>
+        <h3>
+          Sou estudante de desenvolvimento web na
+          {' '}
+          <strong>
+            <a className="trybe" href="https://www.betrybe.com/" target="_blank" rel="noreferrer" style={ { textDecoration: 'none' } }>
+              Trybe
+            </a>
+          </strong>
+          .
+        </h3>
       </div>
-    );
-  }
+    </div>
+  );
 }
