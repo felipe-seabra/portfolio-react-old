766d8cb5280bc0c51d5e349f588c6b7385de82ce
Felipe Seabra <feliperoce@gmail.com>
2022-12-16 10:47:07 -0300

    refactor: Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Loading from '../Loading';
-import './style/Card.css';
+import './style/Card.scss';
 
 function Card(props) {
   const { name, image, description, url, isLoaded } = props;
@@ -10,7 +10,7 @@ function Card(props) {
       {!isLoaded ? (
         <Loading />
       ) : (
-        <div className="card-bg">
+        <div>
           <img className="card-img-top" src={ image } alt={ name } />
           <div className="card-body">
             <h3 className="card-title">{name}</h3>
