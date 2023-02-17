d235972f8554ed98d58c81cf882bd77017fba5f8
Felipe Seabra <feliperoce@gmail.com>
2023-02-17 17:34:58 -0300

    refactor: data

--- src/data/index.js
+++ src/data/index.js
@@ -1,66 +1,66 @@
 /* eslint-disable max-len */
-import criarte from '../images/projects/background-criarte-profissionalizante.png';
-import pixelArt from '../images/projects/background-pixels-art.png';
-import tryunfo from '../images/projects/background-project-tryunfo.png';
-import shoppingCart from '../images/projects/background-shopping-cart.png';
-import trybewarts from '../images/projects/background-project-trybewarts.png';
-import trybewallet from '../images/projects/background-trybewallet.png';
-import trivia from '../images/projects/background-trivia.png';
-import recipeApp from '../images/projects/background-recipe-app.png';
+import criarteImg from '../images/projects/background-criarte-profissionalizante.png';
+import pixelArtImg from '../images/projects/background-pixels-art.png';
+import tryunfoImg from '../images/projects/background-project-tryunfo.png';
+import shoppingCartImg from '../images/projects/background-shopping-cart.png';
+import trybewartsImg from '../images/projects/background-project-trybewarts.png';
+import trybewalletImg from '../images/projects/background-trybewallet.png';
+import triviaImg from '../images/projects/background-trivia.png';
+import recipeAppImg from '../images/projects/background-recipe-app.png';
 
 const data = [
   {
     name: 'Recipe App',
-    image: recipeApp,
+    image: recipeAppImg,
     description:
       'Projeto de desenvolvimento de um App de Receitas com filtros de busca e algumas funcionalidades. 🧑‍🍳 🍲 🍰 ',
     url: 'https://github.com/felipe-seabra/Trybe-project-recipes-app',
   },
   {
     name: 'Trivia',
-    image: trivia,
+    image: triviaImg,
     description:
       'Projeto de desenvolvimento de um jogo de perguntas e respostas baseado no jogo Trivia. 🗣️ 🎙️ 🤵 ✅ ❌',
     url: 'https://github.com/felipe-seabra/Trybe-project-trivia',
   },
   {
     name: 'Trybewallet',
-    image: trybewallet,
+    image: trybewalletImg,
     description:
       'Projeto de desenvolvimento de uma carteira de controle de gastos com conversor de moedas. 📖 🖋️ 💵 💶 ₿',
     url: 'https://github.com/felipe-seabra/Trybe-project-trybewallet',
   },
   {
     name: 'Tryunfo',
-    image: tryunfo,
+    image: tryunfoImg,
     description:
       'Projeto de desenvolvimento de um jogo no estilo Super Trunfo podendo criar cartas de qualquer tema. 😃🃏',
     url: 'https://github.com/felipe-seabra/Trybe-project-tryunfo',
   },
   {
     name: 'Shopping Cart',
-    image: shoppingCart,
+    image: shoppingCartImg,
     description:
       'Projeto de desenvolvimento de um carrinho de compras utilizando a API do Mercado Livre. 🛒💻📱',
     url: 'https://github.com/felipe-seabra/Trybe-project-shopping-cart',
   },
   {
     name: 'Trybewarts',
-    image: trybewarts,
+    image: trybewartsImg,
     description:
       'Projeto de desenvolvimento de formulário da Escola de Magia de Trybewarts. 🧙🪄⚡',
     url: 'https://github.com/felipe-seabra/Trybe-project-trybewarts',
   },
   {
     name: 'Pixels Art',
-    image: pixelArt,
+    image: pixelArtImg,
     description:
       'Projeto de desenvolvimento de uma paleta de cores para criar desenhos em pixels. 🎨🖌',
     url: 'https://github.com/felipe-seabra/Trybe-project-pixels-art',
   },
   {
     name: 'Página Colégio Criarte',
-    image: criarte,
+    image: criarteImg,
     description:
       'Projeto desenvolvido para descrição dos cursos profissionalizantes do Colégio Criarte.',
     url: 'https://github.com/felipe-seabra/criarteProfissionalizante',
