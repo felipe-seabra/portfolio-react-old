4e301c577b33f1581c9b307f246ca2d5c80a3a1e
Felipe Seabra <feliperoce@gmail.com>
2023-02-17 17:24:07 -0300

    refactor: Projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -9,31 +9,35 @@ export default function Projects() {
 
   useEffect(() => {
     setPageTitle('Projetos - Felipe Seabra');
-    const TOMEOUT_LIMIT = 800;
-    setTimeout(() => {
-      setIsLoaded(true);
-    }, TOMEOUT_LIMIT);
+    const TIMEOUT_LIMIT = 800;
+    const timeoutId = setTimeout(() => setIsLoaded(true), TIMEOUT_LIMIT);
+
+    return () => clearTimeout(timeoutId);
   }, []);
+
+  const renderCards = () => {
+    if (data.length < 1) {
+      return <p>Nenhum projeto encontrado</p>;
+    }
+
+    return data.map((card) => (
+      <Card
+        key={ card.name }
+        image={ card.image }
+        name={ card.name }
+        description={ card.description }
+        url={ card.url }
+        isLoaded={ isLoaded }
+      />
+    ));
+  };
 
   return (
     <div className="background-color-grey projects">
       <div className="projects__title">
         <h2>Meus Projetos</h2>
       </div>
-      <div className="projects__card-box">
-        {data.length < 1
-          ? <p>Nenhum projeto encontrado</p>
-          : data.map((card) => (
-            <Card
-              key={ card.name }
-              image={ card.image }
-              name={ card.name }
-              description={ card.description }
-              url={ card.url }
-              isLoaded={ isLoaded }
-            />
-          ))}
-      </div>
+      <div className="projects__card-box">{renderCards()}</div>
     </div>
   );
 }
