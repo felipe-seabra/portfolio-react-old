9a5cfe69ae878ca6c1014984decf19ef2cfec0a7
Felipe Seabra <feliperoce@gmail.com>
2023-02-17 17:26:35 -0300

    refactor: Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,34 +3,33 @@ import PropTypes from 'prop-types';
 import Loading from '../Loading';
 import './style/Card.scss';
 
-function Card(props) {
-  const { name, image, description, url, isLoaded } = props;
+function Card({ name, image, description, url, isLoaded }) {
   return (
     <div id="card">
       {!isLoaded ? (
         <Loading />
       ) : (
-        <div>
+        <>
           <img src={ image } alt={ name } />
           <div className="card-body">
             <h3>{name}</h3>
             <p>{description}</p>
-            <a href={ url } target="_blank" className="btn mt-2" rel="noreferrer">
+            <a href={ url } target="_blank" rel="noreferrer" className="btn mt-2">
               Visitar Repositório
             </a>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
 }
 
 Card.propTypes = {
-  name: PropTypes.string,
-  image: PropTypes.string,
-  description: PropTypes.string,
-  url: PropTypes.string,
-  isLoaded: PropTypes.bool,
-}.isRequired;
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  isLoaded: PropTypes.bool.isRequired,
+};
 
 export default Card;
