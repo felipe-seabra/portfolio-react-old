fe01914e649e7eeabacb2495d6728d0a09cc852c
Felipe Seabra <feliperoce@gmail.com>
2023-02-17 17:30:02 -0300

    refactor: Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,38 +1,27 @@
 import React from 'react';
 import './style/Footer.scss';
 
-function Footer() {
-  const instagram = 'https://www.instagram.com/felipeseabra_';
-  const linkedin = 'https://www.linkedin.com/in/felipe-seabra';
-  const github = 'https://github.com/felipe-seabra';
+const SOCIAL_NETWORKS = [
+  { url: 'https://github.com/felipe-seabra', iconClass: 'fab fa-github' },
+  { url: 'https://www.linkedin.com/in/felipe-seabra', iconClass: 'fab fa-linkedin' },
+  { url: 'https://www.instagram.com/felipeseabra_', iconClass: 'fab fa-instagram' },
+];
 
+function Footer() {
   return (
     <footer>
       <div className="social-networks">
-        <a
-          target="_blank"
-          href={ github }
-          rel="noreferrer"
-          style={ { textDecoration: 'none' } }
-        >
-          <i className="fab fa-github" />
-        </a>
-        <a
-          target="_blank"
-          href={ linkedin }
-          rel="noreferrer"
-          style={ { textDecoration: 'none' } }
-        >
-          <i className="fab fa-linkedin" />
-        </a>
-        <a
-          target="_blank"
-          href={ instagram }
-          rel="noreferrer"
-          style={ { textDecoration: 'none' } }
-        >
-          <i className="fab fa-instagram" />
-        </a>
+        {SOCIAL_NETWORKS.map((network, index) => (
+          <a
+            key={ index }
+            target="_blank"
+            href={ network.url }
+            rel="noreferrer"
+            style={ { textDecoration: 'none' } }
+          >
+            <i className={ network.iconClass } />
+          </a>
+        ))}
       </div>
       <p className="end">Felipe S. - &copy; 2022</p>
     </footer>
